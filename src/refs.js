6e3fcbefd6250521057d15f22b03b1cf7e085238
Serhii Zhyhanov <zhiganov76@gmail.com>
2022-08-29 10:32:23 +0300

    виконав ДЗ, чорнова

---
+++ src/refs.js
@@ -0,0 +1,5 @@
+export const refs = {
+  formEl: document.querySelector('.search-form'),
+  galleryEl: document.querySelector('.gallery'),
+  buttonLoadMore: document.querySelector('.load-more'),
+};
